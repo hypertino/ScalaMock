56f24ba2572957a9bdc5d0cf7281ea60748acf8d
Maga Abdurakhmanov <maqdev@gmail.com>
2016-09-25 03:07:53 +0200

    * scala 2.10 compatibility

--- travis/build.sh
+++ travis/build.sh
@@ -6,9 +6,9 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "scalajs-hypertin
   echo "$key_password" | gpg --passphrase-fd 0 ./travis/ht-oss-private.asc.gpg
 
   if grep "buildVersion\s*=.*-SNAPSHOT" build.sbt; then
-    sbt test +coreJVM/publishSigned +coreJS/publishSigned +scalatestSupportJVM/publishSigned +scalatestSupportJS/publishSigned
+    sbt +test +coreJVM/publishSigned +coreJS/publishSigned +scalatestSupportJVM/publishSigned +scalatestSupportJS/publishSigned
   else
-    sbt test +coreJVM/publishSigned +coreJS/publishSigned +scalatestSupportJVM/publishSigned +scalatestSupportJS/publishSigned sonatypeReleaseAll
+    sbt +test +coreJVM/publishSigned +coreJS/publishSigned +scalatestSupportJVM/publishSigned +scalatestSupportJS/publishSigned sonatypeReleaseAll
   fi
 else
   sbt test
